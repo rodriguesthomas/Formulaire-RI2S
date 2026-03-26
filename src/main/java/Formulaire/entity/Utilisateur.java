4f04b39aed45ee5895212fda23c59e0392b752f0
rodriguesthomas2005-ai <thomas.rodrigues_da_conceicao@etud.univ-jfc.fr>
2026-03-26 14:17:46 +0100

    26/03

--- src/main/java/Formulaire/entity/Utilisateur.java
+++ src/main/java/Formulaire/entity/Utilisateur.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -52,11 +53,11 @@ public class Utilisateur {
     @Column(nullable = false)
     private Date dateNaissance;
 
-    @OneToOne(mappedBy = "utilisateur", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "utilisateur", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JsonManagedReference
     private Professionnel profilPro;
 
-    @OneToOne(mappedBy = "utilisateur", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "utilisateur", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JsonManagedReference
     private NonProfessionnel profilNonPro;
 
