de920ae96f7f2c8ffb4e206d3ead3b622e8c9cb1
rodriguesthomas2005-ai <thomas.rodrigues_da_conceicao@etud.univ-jfc.fr>
2026-03-26 09:43:27 +0100

    26/03

--- src/main/java/Formulaire/entity/NonProfessionnel.java
+++ src/main/java/Formulaire/entity/NonProfessionnel.java
@@ -1,10 +1,17 @@
 package Formulaire.entity;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,7 +21,6 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-
 @Entity
 @Table(name = "non_professionnel")
 @Data
@@ -30,11 +36,11 @@ public class NonProfessionnel {
     @JsonBackReference 
     private Utilisateur utilisateur;
 
-    // @ElementCollection(targetClass = MomentJournee.class)
-    // @CollectionTable(name = "utilisateur_moments", joinColumns = @JoinColumn(name = "id_utilisateur"))
-    // @Enumerated(EnumType.STRING)
-    // @Column(name = "moment")
-    // private List<MomentJournee> momentsJournee;
+    @ElementCollection(fetch = FetchType.EAGER) 
+    @CollectionTable(name = "utilisateur_moments", joinColumns = @JoinColumn(name = "id_utilisateur"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "moment")
+    private List<MomentJournee> momentsJournee;
 
     @NonNull
     @Column(nullable = false)
