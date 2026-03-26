b3f71fd13875f07201715b022c574075d9881159
rodriguesthomas2005-ai <thomas.rodrigues_da_conceicao@etud.univ-jfc.fr>
2026-03-26 09:18:35 +0100

    26/03

--- src/main/java/Formulaire/controller/UtilisateurController.java
+++ src/main/java/Formulaire/controller/UtilisateurController.java
@@ -68,7 +68,6 @@ public class UtilisateurController {
             map.put("consentement", u.getConsentement());
             map.put("telephone", u.getTelephone());
             map.put("email", u.getEmail());
-            map.put("profilNonPro", u.getProfilNonPro());
             return map;
         }).toList();
     }
